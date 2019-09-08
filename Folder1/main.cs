fdfbf24b23d6a04f463d70df0c7b588472f6d5a5
lcf007 <lcf007@msn.com>
2019-09-08 16:07:15 -0400

    Use it in main.

--- Folder1/main.cs
+++ Folder1/main.cs
@@ -1,7 +1,12 @@
 // This is my first cs file.
+
+using UploadFiles;
+
 namespace Main
 {
     public class MainClass{
+        UploadFiles uf = new UploadFiles();
+
         static void Main(){
             System.Console.WriteLine("Why me??");
         }
