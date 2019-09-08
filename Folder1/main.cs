443e1ff1e7cbd2da483f0cae525ab6f933a6a7b4
lcf007 <lcf007@msn.com>
2019-09-08 15:34:05 -0400

    Change Main

--- Folder1/main.cs
+++ Folder1/main.cs
@@ -1,1 +1,10 @@
 // This is my first cs file.
+namespace Main
+{
+    public class MainClass{
+        static void Main(){
+            System.Console.WriteLine("Why me??");
+        }
+    }
+    
+}
