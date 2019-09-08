2528b10df4d9f3b11020c848fa98878340c0a290
lcf007 <lcf007@msn.com>
2019-09-08 16:08:25 -0400

    Using Login Feature.

--- Folder1/main.cs
+++ Folder1/main.cs
@@ -1,6 +1,7 @@
 // This is my first cs file.
 namespace Main
 {
+    LoginFeature lf = new LoginFeature();
     public class MainClass{
         static void Main(){
             System.Console.WriteLine("Why me??");
